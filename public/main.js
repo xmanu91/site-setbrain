445a28f558dbf98b3f543a145d7de99ce8affde9
xmanu91 <65608769+xmanu91@users.noreply.github.com>
2020-12-30 14:42:28 +0100

    rectif responsive

--- public/main.js
+++ public/main.js
@@ -1,57 +1,37 @@
 const isOpac = true;
-const isDefil= true;
+const isDefil = true;
 
- window.addEventListener("scroll", (event) => {
-   let scroll = this.scrollY;
-   
-   if(scroll > 50 && isOpac){
-     
-     var container = document.querySelector("#sec-2 .maincontainer");
-     container.style.opacity = 1.0;
-     this.isOpac = false;
-     }
+window.addEventListener("scroll", (event) => {
+  let scroll = this.scrollY;
 
-    if(scroll > 50){
-      var social = document.querySelector(".socialcontainer");
-      social.classList.add('active');
-    }
-
-    if(scroll < 50){
-      var social = document.querySelector(".socialcontainer");
-      social.classList.remove('active');
-    }
-
-
-  if(scroll > 999 && isDefil){
-    var defil = document.querySelector('#sec-3 .maincontainer');
-    defil.classList.add('active');
-    this.isDefil = false;
+  if (scroll > 50 && isOpac) {
+    var container = document.querySelector("#sec-2 .maincontainer");
+    container.style.opacity = 1.0;
+    this.isOpac = false;
   }
 
- });
+  if (scroll > 50) {
+    var social = document.querySelector(".socialcontainer");
+    social.classList.add("active");
+  }
 
- if(screen.width <= 1200){
-   var right = document.querySelector('#sec-3 .left');
+  if (scroll < 50) {
+    var social2 = document.querySelector(".socialcontainer");
+    social2.classList.remove("active");
+  }
 
-   right.remove();
-   
-   var right2 = document.querySelector('#sec-1 .right');
-  right2.remove();
-  
-   var right3 = document.querySelector('#sec-2 svg');
-   right3.remove();
-   
-   
-   var right4 = document.querySelector('#sec-3 .right h1');
+  if (scroll > 999 && isDefil) {
+    var defil = document.querySelector("#sec-3 .maincontainer");
+    defil.classList.add("active");
+    this.isDefil = false;
+  }
+});
 
-   right4.remove();
- }
- 
- 
- 
- function round(number){
-   var cont = document.querySelector(".maincontainer .container");
-          
-    cont.style.left = number + '%';
- }
- 
+function round(number) {
+  var cont = document.querySelector(".maincontainer .container");
+
+  cont.style.left = number + "%";
+}
+
+var defaultFocus = document.getElementById("focus");
+defaultFocus.focus({ preventScroll: true });
