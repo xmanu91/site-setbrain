8249c96428050e18a2d61eaf4096d29b1b938a8d
xmanu91 <65608769+xmanu91@users.noreply.github.com>
2021-03-10 13:36:02 +0000

    terminal

--- public/laxmain.js
+++ public/laxmain.js
@@ -1,9 +1,8 @@
-window.onload = function () {
-  lax.setup(); // init
-
-  const updateLax = () => {
-    lax.update(window.scrollY);
-    window.requestAnimationFrame(updateLax);
-  };
-  window.requestAnimationFrame(updateLax);
-};
+var rellax = new Rellax('.rellax', {
+  speed: -400,
+  center: false,
+  wrapper: null,
+  round: true,
+  vertical: true,
+  horizontal: false
+});
